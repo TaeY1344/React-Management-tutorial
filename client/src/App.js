94fd50555f726c460b2c0fb650538723bec71bde
TaeY1344 <xodus1344@gmail.com>
2020-08-02 12:46:48 +0900

    Add Get Customers REST Api Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,31 +20,26 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  id: 1,
-  image: 'https://source.unsplash.com/random/64x64?sig=1',
-  name: '홍길동',
-  birthday: '950930',
-  gender: '남자',
-  job: '프로그래머'
-}, {
-  id: 2,
-  image: 'https://source.unsplash.com/random/64x64?sig=2',
-  name: '심청이',
-  birthday: '940612',
-  gender: '여자',
-  job: '디자이너'
-}, {
-  id: 3,
-  image: 'https://source.unsplash.com/random/64x64?sig=3',
-  name: '철수',
-  birthday: '980715',
-  gender: '남자',
-  job: '대학생'
-}
-]
+
 
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -62,11 +57,10 @@ class App extends React.Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
-                return (
-                  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-                );
+              this.state.customers ? this.state.customers.map(c => {
+                return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
               })
+                : ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: 'https://source.unsplash.com/random/64x64?sig=1',
+      name: '홍길동',
+      birthday: '950930',
+      gender: '남자',
+      job: '프로그래머'
+    }, {
+      id: 2,
+      image: 'https://source.unsplash.com/random/64x64?sig=2',
+      name: '심청이',
+      birthday: '940612',
+      gender: '여자',
+      job: '디자이너'
+    }, {
+      id: 3,
+      image: 'https://source.unsplash.com/random/64x64?sig=3',
+      name: '철수',
+      birthday: '980715',
+      gender: '남자',
+      job: '대학생'
+    }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
